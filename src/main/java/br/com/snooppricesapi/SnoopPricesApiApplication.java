b3b73b36a151a3679e04932ed819a3c4b1e02781
Gabriel Inácio <gabriel.incio.oliveira@gmail.com>
2022-12-04 10:46:42 -0300

    Initial Commit

---
+++ src/main/java/br/com/snooppricesapi/SnoopPricesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.snooppricesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnoopPricesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnoopPricesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/snooppricesapi/SnoopPricesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.snooppricesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnoopPricesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
